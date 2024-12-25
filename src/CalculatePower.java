19307e9fcdd347efddb9eace629c8e6c0d1cdb3d
Hemant Rajpoot <hemantrajpoot56@gmail.com>
2024-12-25 18:01:28 +0530

    Hello

--- src/CalculatePower.java
+++ src/CalculatePower.java
@@ -1,12 +1,13 @@
 public class CalculatePower {
-    static void powerCal(int num,int pow){
+    static double powerCal(int num,int pow){
         int resNum=1;
         for(int i=0;i<pow;i++){
             resNum=resNum*num;
         }
-        System.out.println(resNum);
+        return resNum;
     }
     public static void main(String[] args) {
-        powerCal(10,2);
+        double value = powerCal(10,2);
+        System.out.println(value);
     }
 }
