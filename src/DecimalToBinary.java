d3111e0cfb7bfdf45377ffc7004431850f668527
Hemant Rajpoot <hemantrajpoot56@gmail.com>
2024-12-25 16:55:42 +0530

    ad

--- src/DecimalToBinary.java
+++ src/DecimalToBinary.java
@@ -2,6 +2,7 @@ public class DecimalToBinary {
 
     public static void main(String[] args) {
         decimaltobinary(5);
+        decimaltoAnybase(10,2);
     }
 
     static void decimaltobinary(int decimalnum){
@@ -15,5 +16,15 @@ public class DecimalToBinary {
         }
         System.out.println("Decimal to binary : " + resNum);
     }
-
+    static void decimaltoAnybase(int decimalnum, int base) {
+        int resNum = 0;
+        int power = 0;
+        while (decimalnum > 0) {
+            int rem = decimalnum % base;
+            decimalnum /= base;
+            resNum += rem * Math.pow(10, power);
+            power++;
+        }
+        System.out.println("Decimal to Base : " + resNum);
+    }
 }
